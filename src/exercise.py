b941040f8f5061f29508926c6cbc8b32d1b3030f
Vladimir Slysar <g7au2ss@gmail.com>
2020-07-20 01:20:39 +0700

    1

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,18 @@
+class Example(object):
+    """docstring fo Example."""
+
+    def __init__(self, *args):
+        self.print_text()
+
+    def print_text(self):
+        print("In a hole in the ground there lived a method")
+
+
 def main():
     #write your code below this line
+    num = int(input("How many times?"))
+    for i in range(num):
+        x = Example()
 
 if __name__ == '__main__':
     main()
